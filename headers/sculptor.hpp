3d58f544c0d2d0a9da065fad91b86ebe00ac4a0d
Ariadne Evangelista <84780402+ariadnec-es@users.noreply.github.com>
2022-07-22 00:35:55 -0300

    Update sculptor.hpp

--- headers/sculptor.hpp
+++ headers/sculptor.hpp
@@ -1,23 +1,23 @@
 #ifndef SCULPTOR_H
 #define SCULPTOR_H
-
+#include <vector>
 #include "voxel.hpp"
 
 class Sculptor {
 protected:
   Voxel ***v; // ponteiro para ponteiro para ponteiro do tipo struct Voxel
 
-  int nx,ny,nz; // dimensıes x,y e z da matriz
+  int nx,ny,nz; // dimens√µes x,y e z da matriz
   float r,g,b,a; // define as cores vermelho, verde e azul para cada voxel, e transparencia
-  float fix; //vari·vel de valor fixo para auxilio da construÁ„o do arquivo .off
+  float fix; //vari√°vel de valor fixo para auxilio da constru√ß√£o do arquivo .off
 
 public:
-  Sculptor(int _nx, int _ny, int _nz); //construtor respons·vel por iniciar classe
+  Sculptor(int _nx, int _ny, int _nz); //construtor respons√°vel por iniciar classe
   ~Sculptor(); // destrutor da classe
 
-  void setColor(float r, float g, float b, float alpha); // se refere a seleÁ„o de cor
-  void putVoxel(int x, int y, int z); // se refere a ativaÁ„o de um voxel em (x,y,z)
-  void cutVoxel(int x, int y, int z); // se refere a desativaÁ„o de um voxel em (x,y,z)
+  void setColor(float r, float g, float b, float alpha); // se refere a sele√ß√£o de cor
+  void putVoxel(int x, int y, int z); // se refere a ativa√ß√£o de um voxel em (x,y,z)
+  void cutVoxel(int x, int y, int z); // se refere a desativa√ß√£o de um voxel em (x,y,z)
 
   int getx();
   int gety();
